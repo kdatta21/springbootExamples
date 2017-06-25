5814930168d1b4c564dc820442b4194949c6d5e8
kaushik <kdatta21@in.ibm.com>
2017-06-25 11:40:32 +0530

    form submission with bug

--- src/main/java/controller/GreetingController.java
+++ src/main/java/controller/GreetingController.java
@@ -1,22 +1,24 @@
 package controller;
 
-import java.util.concurrent.atomic.AtomicLong;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import model.Greeting;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 @RestController
 public class GreetingController {
 
-	private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
+	@GetMapping("/greeting")
+    public String greetingForm(Model model) {
+        model.addAttribute("greeting", new Greeting());
+        return "greeting";
+    }
 
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+    @PostMapping("/greeting")
+    public String greetingSubmit(@ModelAttribute Greeting greeting) {
+        return "result";
     }
 }
--- src/main/java/model/Greeting.java
+++ src/main/java/model/Greeting.java
@@ -2,19 +2,23 @@ package model;
 
 public class Greeting {
 
-	private final long id;
-    private final String content;
-
-    public Greeting(long id, String content) {
-        this.id = id;
-        this.content = content;
-    }
+	private long id;
+    private String content;
 
     public long getId() {
         return id;
     }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
     public String getContent() {
         return content;
     }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
 }
